e00b90fc73daddf71cc00f418774ca400a0a9742
SnowzTail <i@snowztail.com>
2023-09-01 16:25:21 +0100

    :construction: Test conjugate gradient algorithm

--- src/test.m
+++ src/test.m
@@ -34,24 +34,32 @@ function [Theta] = update_ris(H_d, H_f, H_b, Q, P_n)
 	iter = create_iterator;
 	while ~iter.converged && ~iter.timeout
 		H = channel_aggregate(H_d, H_f, H_b, Theta);
-		R = rate_mimo(H, Q, P_n);
-		
 		G_e = (H_b') / (eye(size(H, 1)) + H * (Q / P_n) * H') * (H * (Q / P_n) * H_f');
 		G_r = G_e * Theta' - Theta * G_e';
-		D = G_r;
 		
-		% * Armijo step size
+		if mod(iter.counter, numel(Theta)) == 0
+			D = G_r;
+		else
+			gamma = trace((G_r - prev.G_r) * G_r') / trace(prev.G_r * prev.G_r');
+			D = G_r + gamma * prev.D;
+			if real(trace(D' * G)) < 0
+				D = G_r;
+			end
+		end
+		
+		iter.converged = (0.5 * trace(G_r * G_r')) <= iter.minGap;
+		if iter.converged
+			break;
+		end
+		
 		mu = step_armijo(H_d, H_f, H_b, Theta, Q, P_n, D);
+		Theta = expm(-mu * D) * Theta;
 		
-		
-		% * Conjugate direction
 		H = channel_aggregate(H_d, H_f, H_b, Theta);
 		R = rate_mimo(H, Q, P_n);
-		
-		G_e = (H_b') / (eye(size(H, 1)) + H * (Q / P_n) * H') * (H * (Q / P_n) * H_f');
-		G_r = G_e * Theta' - Theta * G_e';
-		% gamma = trace(G_r -)
 	end
+	
+	init.Theta = Theta;
 end
 
 function [mu] = step_armijo(H_d, H_f, H_b, Theta, Q, P_n, D)
@@ -60,69 +68,34 @@ function [mu] = step_armijo(H_d, H_f, H_b, Theta, Q, P_n, D)
 	
 	test.mu = -1;
 	test.T = expm(-test.mu * D);
-	test.T2 = test.T ^ 2;
 	
-	test.Theta = test.T2 * Theta;
-	test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
+	test.H = channel_aggregate(H_d, H_f, H_b, test.T ^ 2 * Theta);
 	test.R = rate_mimo(test.H, Q, P_n);
 	while (R - test.R) <= (test.mu * 0.5 * trace(D * D'))
-		test.T = test.T2;
-		test.T2 = test.T ^ 2;
 		test.mu = test.mu * 2;
-		test.Theta = test.T2 * Theta;
-		test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
+		% test.T = expm(-test.mu * D);
+		test.T = test.T ^ 2;
+		
+		test.H = channel_aggregate(H_d, H_f, H_b, test.T ^ 2 * Theta);
 		test.R = rate_mimo(test.H, Q, P_n);
 	end
 	
-	test.Theta = test.T * Theta;
-	test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
+	test.H = channel_aggregate(H_d, H_f, H_b, test.T * Theta);
 	test.R = rate_mimo(test.H, Q, P_n);
 	while (R - test.R) > (0.5 * test.mu * 0.5 * trace(D * D'))
-		test.T = expm(-test.mu * D);
 		test.mu = test.mu * 0.5;
-		test.Theta = test.T * Theta;
-		test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
+		test.T = expm(-test.mu * D);
+		% test.T = test.T ^ 0.5;
+		
+		test.H = channel_aggregate(H_d, H_f, H_b, test.T * Theta);
 		test.R = rate_mimo(test.H, Q, P_n);
 	end
 	
 	mu = test.mu;
 end
 
-
-% function [mu] = step_armijo(H_d, H_f, H_b, Q, Theta, G_r, D, P_n)
-% 	H = channel_aggregate(H_d, H_f, H_b, Theta);
-% 	R = rate_mimo(H, Q, P_n);
-
-% 	test.mu = -1;
-% 	test.T = expm(-test.mu * D);
-% 	test.T2 = test.T ^ 2;
-
-% 	test.Theta = test.T2 * Theta;
-% 	test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
-% 	test.R = rate_mimo(test.H, Q);
-% 	while (R - test.R) <= (test.mu * 0.5 * trace(D * D'))
-% 		test.T = test.T2;
-% 		test.T2 = test.T ^ 2;
-% 		test.mu = test.mu * 2;
-% 		test.Theta = test.T2 * Theta;
-% 		test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
-% 		test.R = rate_mimo(test.H, Q);
-% 	end
-
-% 	test.Theta = test.T * Theta;
-% 	test.H = channel_aggregate(H_d, H_f, H_b, test.Theta);
-% 	test.R = rate_mimo(test.H, Q);
-% 	while (R - test.R) > (0.5 * test.mu * 0.5 * trace(D * D'))
-% 		test.T = expm(-test.mu * D);
-% 		test.mu = test.mu * 0.5;
-% 	end
-
-% 	flag = 1;
-% end
-
-
 function [iterator] = create_iterator
 	[iterator.converged, iterator.timeout] = deal(false);
-	[iterator.minGap, iterator.maxRun] = deal(1e-2, 1e2);
+	[iterator.minGap, iterator.maxRun] = deal(1e-4, 1e2);
 	[iterator.reference, iterator.counter] = deal(0);
 end
