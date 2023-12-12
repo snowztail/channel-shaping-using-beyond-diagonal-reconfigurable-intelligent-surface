c1f3c2bba79a4a0b6d38bebdadc75de47f810cbf
SnowzTail <i@snowztail.com>
2023-12-12 02:01:59 +0000

    :construction: Refactoring

--- src/ic_rate_sx.m
+++ src/ic_rate_sx.m
@@ -1,8 +1,10 @@
 clc; clear; close; setup;
 
-[transmit.antenna, reflect.antenna, receive.antenna, transmit.stream, network.pair] = deal(8, 2 .^ [5, 8], 4, 3, 3);
-[transmit.power, receive.noise, receive.weight] = deal(db2pow(-20), db2pow(-75 : -20 : -115), ones(network.pair, 1));
-[channel.pathloss.direct, channel.pathloss.forward, channel.pathloss.backward] = deal(db2pow(-65), db2pow(-54), db2pow(-46));
+[transmit.antenna, reflect.antenna, receive.antenna, transmit.stream, network.pair] = deal(4, 2 .^ [2, 4, 6, 8], 2, 2, 3);
+% [transmit.power, receive.noise, receive.weight] = deal(db2pow(-20), db2pow(-75 : -20 : -115), ones(network.pair, 1));
+% [channel.pathloss.direct, channel.pathloss.forward, channel.pathloss.backward] = deal(db2pow(-65), db2pow(-54), db2pow(-46));
+[transmit.power, receive.noise, receive.weight] = deal(db2pow(0), db2pow(-10 : -10 : -40), ones(network.pair, 1));
+[channel.pathloss.direct, channel.pathloss.forward, channel.pathloss.backward] = deal(db2pow(0), db2pow(-10), db2pow(-10));
 [number.bond, number.noise, number.antenna, number.realization] = deal(2, length(receive.noise), length(reflect.antenna), 1e1);
 
 for r = 1 : number.realization
@@ -10,29 +12,25 @@ for r = 1 : number.realization
 	for a = 1 : number.antenna
 		channel.forward = sqrt(channel.pathloss.forward) .* fading_nlos(reflect.antenna(a), transmit.antenna, 1, network.pair);
 		channel.backward = sqrt(channel.pathloss.backward) .* fading_nlos(receive.antenna, reflect.antenna(a), network.pair, 1);
-		transmit.beamformer = precoder_initial_ic(channel.direct, transmit.stream, transmit.power);
-		for n = 1 : number.noise
-			if reflect.antenna(a) == 0
-				transmit.beamformer = precoder_rate_ic(channel.direct, transmit.beamformer, transmit.power, receive.noise(n), receive.weight);
-				receive.rate(1, n, a, r) = receive.weight' * rate_mimo_ic(channel.direct, transmit.beamformer, receive.noise(n));
-			else
-				reflect.bond = [1, reflect.antenna(a)];
-				reflect.beamformer = eye(reflect.antenna(a));
-				% transmit.beamformer = precoder_initial_ic(channel.direct, transmit.stream, transmit.power);
-				for b = 1 : number.bond
-					channel.aggregate = channel_aggregate(channel.direct, channel.forward, channel.backward, reflect.beamformer);
-					[iter.converge, iter.tolerance, iter.counter] = deal(false, 1e-3, 0);
-					iter.rate = receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n));
-					while ~iter.converge
-						[reflect.beamformer, channel.aggregate] = scatter_rate_ic(channel.direct, channel.forward, channel.backward, transmit.beamformer, reflect.beamformer, reflect.bond(b), receive.noise(n), receive.weight);
-receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n))
-						transmit.beamformer = precoder_rate_ic(channel.aggregate, transmit.beamformer, transmit.power, receive.noise(n), receive.weight);
-receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n))
-						receive.rate(b, n, a, r) = receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n));
-						iter.converge = (abs(receive.rate(b, n, a, r) - iter.rate) / iter.rate <= iter.tolerance);
-						iter.rate = receive.rate(b, n, a, r);
-						iter.counter = iter.counter + 1;
-					end
+		reflect.bond = [1, reflect.antenna(a)];
+		for b = 1 : number.bond
+			reflect.beamformer = eye(reflect.antenna(a));
+			% foo = repmat({dftmtx(reflect.bond(b))}, [reflect.antenna(a) / reflect.bond(b), 1]);
+			% reflect.beamformer = blkdiag(foo{:}) / sqrt(reflect.bond(b));
+			channel.aggregate = channel_aggregate(channel.direct, channel.forward, channel.backward, reflect.beamformer);
+			transmit.beamformer = precoder_initial_ic(channel.aggregate, transmit.stream, transmit.power);
+			for n = 1 : number.noise
+				[iter.converge, iter.tolerance, iter.counter] = deal(false, 1e-5, 0);
+				iter.rate = receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n));
+				while ~iter.converge
+					transmit.beamformer = precoder_rate_ic(channel.aggregate, transmit.beamformer, transmit.power, receive.noise(n), receive.weight);
+	receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n))
+					[reflect.beamformer, channel.aggregate] = scatter_rate_ic(channel.direct, channel.forward, channel.backward, transmit.beamformer, reflect.beamformer, reflect.bond(b), receive.noise(n), receive.weight);
+	receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n))
+					receive.rate(n, b, a, r) = receive.weight' * rate_mimo_ic(channel.aggregate, transmit.beamformer, receive.noise(n));
+					iter.converge = (abs(receive.rate(n, b, a, r) - iter.rate) / iter.rate <= iter.tolerance);
+					iter.rate = receive.rate(n, b, a, r);
+					iter.counter = iter.counter + 1;
 				end
 			end
 		end
---
+++ src/lib/precoder_rate_ic_bisection.m
@@ -0,0 +1,68 @@
+function [W] = precoder_rate_ic_bisection(H, W, P_t, P_n, rho)
+	% [N_r, N_t, N_e, K, P_t] = deal(size(H, 1), size(H, 2), size(W, 2), size(W, 4), pagenorm(W, 'fro') .^ 2);
+	[N_r, N_t, N_e, K] = deal(size(H, 1), size(H, 2), size(W, 2), size(W, 4));
+	[iter.converge, iter.tolerance, iter.counter] = deal(false, 1e-3, 0);
+	iter.J = rho' * rate_mimo_ic(H, W, P_n);
+	while ~iter.converge
+		T = pagemtimes(pagemtimes(H, W), 'none', pagemtimes(H, W), 'ctranspose');
+		Q = sum(T, 4) - T(:, :, logical(eye(K))) + P_n * eye(N_r);
+		F = pagemtimes(H(:, :, logical(eye(K))), pageswap(W));
+		G = pagemrdivide(pagectranspose(F), Q + T(:, :, logical(eye(K))));
+		V = shiftdim(rho, -2) .* pagehermitize(eye(N_e) + pagemtimes(pagemrdivide(pagectranspose(F), Q), F));
+		% V = shiftdim(rho, -2) .* pageinv(eye(N_e) - pagemtimes(G, F));
+		Y = pagemtimes(pagemtimes(pagemtimes(G, H), 'ctranspose', V, 'none'), pagemtimes(G, H));
+		Z = pagehermitize(pagemtimes(pagemtimes(W, 'ctranspose', Y, 'none'), W));
+		lambda = (pageswap(pagetrace(P_n * pagehermitize(pagemtimes(pagemtimes(G, 'ctranspose', V, 'none'), G))) + pagetrace(sum(Z, 4))) - pagetrace(sum(Z, 3))) ./ P_t;
+
+
+		lambda1 = zeros(1, 1, 1, K);
+		for k = 1 : K
+			% fun = @(lambda_k) norm((sum(Y(:, :, :, k), 3) + lambda_k * eye(N_t)) \ H(:, :, k, k)' * G(:, :, k)' * V(:, :, k), 'fro') - P_t(k);
+			fun = @(lambda_k) norm(mldivide(sum(Y(:, :, :, k), 3) + lambda_k * eye(N_t), H(:, :, k, k)' * G(:, :, k)' * V(:, :, k)), 'fro') ^ 2 - P_t;
+			if fun(0) * fun(1e12) > 0
+				lambda1(k) = 0;
+			else
+				lambda1(k) = bisection(fun, 0, 1e12, 1e-6);
+			end
+		end
+
+
+
+		% W = pagemldivide(sum(Y, 3) + lambda .* eye(N_t), pageswap(pagemtimes(pagemtimes(G, H(:, :, logical(eye(K)))), 'ctranspose', V, 'none')));
+		W = pagemldivide(sum(Y, 3) + lambda1 .* eye(N_t), pageswap(pagemtimes(pagemtimes(G, H(:, :, logical(eye(K)))), 'ctranspose', V, 'none')));
+		J = rho' * rate_mimo_ic(H, W, P_n);
+		iter.converge = (abs(J - iter.J) / iter.J <= iter.tolerance) && all(abs(pagenorm(W, 'fro') .^ 2 - P_t) ./ P_t < iter.tolerance);
+		iter.J = J;
+		iter.counter = iter.counter + 1;
+	end
+end
+
+function [b] = pagetrace(A)
+	b = sum(pageeig(A), 1);
+end
+
+function [A] = pageswap(A)
+	A = permute(A, [1, 2, 4, 3]);
+end
+
+function [A] = pagehermitize(A)
+	A = 0.5 * (A + pagectranspose(A));
+end
+
+function [x] = bisection(fun, lb, ub, epsilon)
+	assert(epsilon > 0, 'Tolerance must be non-negative.');
+	assert(lb < ub, 'Lower bound must be smaller than upper bound.');
+	assert(fun(lb) * fun(ub) < 0, 'Initial values must have opposite signs.');
+
+    while (ub - lb) / 2 > epsilon
+        x = (lb + ub) / 2;
+        if fun(x) == 0
+            break
+        elseif fun(x) * fun(lb) < 0
+            ub = x;
+        else
+            lb = x;
+        end
+    end
+    % x = (lb + ub) / 2;
+end
